3e59f44e894b225583956d7e461467f14919b9a3
keyzafirstanto <rfirstanto@gmail.com>
2021-10-08 22:28:17 +0700

    FE-01 admin is able to add new data

---
+++ src/pages/AdminProducts/ModalAddProduct/ModalAddProduct.jsx
@@ -0,0 +1,335 @@
+import React, { useState, useEffect } from 'react';
+import { API_URL } from '../../../helper';
+import Axios from 'axios';
+import './modaladdproduct.css';
+
+export const AddModal = ({ showModal }) => {
+  const [productFetch, setProductFetch] = useState({
+    productDataList: [],
+  });
+
+  const [newProduct, setNewProduct] = useState({
+    addProductName: '',
+    addProductDesc: '',
+    addProductStock: 0,
+    addProductNetto: 0,
+    addProductNettoTotal: 0,
+    addProductUnit: 0,
+    addProductPricePerUnit: 0,
+    addProductPricePerStock: 0,
+    addProductBrand: '',
+    addProductCategory: '',
+  });
+
+  const [addImage, setAddImage] = useState({
+    addFile: '',
+    addFileName: '',
+  });
+
+  const fetchProducts = () => {
+    Axios.get(`${API_URL}/products/get`)
+      .then((res) => {
+        setProductFetch({
+          ...productFetch,
+          productDataList: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const btnAddImage = (e) => {
+    if (e.target.files[0]) {
+      setAddImage({
+        ...addImage,
+        addFileName: e.target.files[0].name,
+        addFile: e.target.files[0],
+      });
+
+      let preview = document.getElementById('imgpreview');
+      preview.src = URL.createObjectURL(e.target.files[0]);
+    }
+  };
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setNewProduct({ ...newProduct, [name]: value });
+  };
+
+  const cancelEdit = () => {
+    setNewProduct({
+      ...newProduct,
+      addProductName: '',
+      addProductDesc: '',
+      addProductStock: 0,
+      addProductNetto: 0,
+      addProductNettoTotal: 0,
+      addProductUnit: 0,
+      addProductPricePerUnit: 0,
+      addProductPricePerStock: 0,
+      addProductBrand: '',
+      addProductCategory: '',
+    });
+  };
+
+  const addNewProduct = () => {
+    if (addImage.addFile) {
+      let formData = new FormData();
+      formData.append(
+        'data',
+        JSON.stringify({
+          product_name: newProduct.addProductName,
+          product_desc: newProduct.addProductDesc,
+          stock: parseInt(newProduct.addProductStock),
+          netto: parseInt(newProduct.addProductNetto),
+          netto_total: parseInt(newProduct.addProductNettoTotal),
+          unit: newProduct.addProductUnit,
+          price_per_unit: parseInt(newProduct.addProductPricePerUnit),
+          price_per_stock: parseInt(newProduct.addProductPricePerStock),
+          brand_id: newProduct.addProductBrand,
+          category_id: newProduct.addProductCategory,
+        })
+      );
+      formData.append('file', addImage.addFile);
+
+      Axios.post(`${API_URL}/products/post`, formData)
+        .then((res) => {
+          alert(res.data.message);
+          fetchProducts();
+          setNewProduct({
+            addProductName: '',
+            addProductDesc: '',
+            addProductStock: 0,
+            addProductNetto: 0,
+            addProductNettoTotal: 0,
+            addProductUnit: 0,
+            addProductPricePerUnit: 0,
+            addProductPricePerStock: 0,
+            addProductBrand: '',
+            addProductCategory: '',
+          });
+          setAddImage({
+            addFile: '',
+            addFileName: '',
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <>
+      {showModal ? (
+        <div className="modal_container">
+          <div className="m-5">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h2>Form</h2>
+              </div>
+              <div className="modal-body">
+                <div>
+                  <div>
+                    <img id="imgpreview" alt="" width="100%" />
+                  </div>
+                  <label for="img" className="text-xl-left">
+                    Add image
+                  </label>
+                  <input
+                    onChange={btnAddImage}
+                    type="file"
+                    className="form-control"
+                    id="img"
+                  />
+                </div>
+                <div>
+                  <label for="productname" className="text-xl-left">
+                    Product Name
+                  </label>
+                  <input
+                    value={newProduct.addProductName}
+                    onChange={inputHandler}
+                    name="addProductName"
+                    type="text"
+                    id="productname"
+                    placeholder="Product Name"
+                    className="form-control"
+                  />
+                </div>
+                <div>
+                  <label for="productdescription" className="text-xl-left">
+                    Product Description
+                  </label>
+                  <textarea
+                    value={newProduct.addProductDesc}
+                    onChange={inputHandler}
+                    name="addProductDesc"
+                    type="text"
+                    id="productdescription"
+                    placeholder="Product Description"
+                    className="form-control"
+                  />
+                </div>
+                <div>
+                  <label for="productstock" className="text-xl-left">
+                    Product Stock
+                  </label>
+                  <input
+                    value={newProduct.addProductStock}
+                    onChange={inputHandler}
+                    name="addProductStock"
+                    id="productstock"
+                    type="number"
+                    placeholder="10"
+                    className="form-control"
+                  />
+                </div>
+                <div>
+                  <label for="productnetto" className="text-xl-left">
+                    Product Netto
+                  </label>
+                  <input
+                    value={newProduct.addProductNetto}
+                    onChange={inputHandler}
+                    name="addProductNetto"
+                    type="number"
+                    id="productnetto"
+                    placeholder="400"
+                    className="form-control"
+                  />
+                </div>
+                <div>
+                  <label for="productnettototal" className="text-xl-left">
+                    Product Netto Total
+                  </label>
+                  <input
+                    value={newProduct.addProductNettoTotal}
+                    onChange={inputHandler}
+                    name="addProductNettoTotal"
+                    type="number"
+                    id="productnettototal"
+                    placeholder="4000"
+                    className="form-control"
+                  />
+                </div>
+                <div>
+                  <label for="productunit" className="text-xl-left">
+                    Product Unit
+                  </label>
+                  <select
+                    value={newProduct.addProductUnit}
+                    onChange={inputHandler}
+                    name="addProductUnit"
+                    type="text"
+                    id="productunit"
+                    placeholder="ml"
+                    className="form-control"
+                  >
+                    <option value="ml">ml</option>
+                    <option value="mg">mg</option>
+                    <option value="tablet">tablet</option>
+                    <option value="kaplet">kaplet</option>
+                  </select>
+                </div>
+                <div>
+                  <label for="productpriceperunit" className="text-xl-left">
+                    Price Per Unit
+                  </label>
+                  <input
+                    value={newProduct.addProductPricePerUnit}
+                    onChange={inputHandler}
+                    name="addProductPricePerUnit"
+                    type="number"
+                    id="productpriceperunit"
+                    placeholder="5000"
+                    className="form-control"
+                  />
+                </div>
+                <div>
+                  <label for="productpriceperstock" className="text-xl-left">
+                    Price Per Stock
+                  </label>
+                  <input
+                    value={newProduct.addProductPricePerStock}
+                    onChange={inputHandler}
+                    name="addProductPricePerStock"
+                    type="number"
+                    id="productpriceperstock"
+                    placeholder="5000"
+                    className="form-control"
+                  />
+                </div>
+                <div>
+                  <label for="productbrand" className="text-xl-left">
+                    Product Brand
+                  </label>
+                  <select
+                    value={newProduct.addProductBrand}
+                    onChange={inputHandler}
+                    name="addProductBrand"
+                    type="number"
+                    id="productbrand"
+                    placeholder="Kimia Farma"
+                    className="form-control"
+                  >
+                    <option value="1">Kalbe Farma</option>
+                    <option value="2">Sanbe Farma</option>
+                    <option value="3">Dexa Medica</option>
+                    <option value="4">Pharos Indonesia</option>
+                    <option value="5">Kimia Farma</option>
+                    <option value="6">Biofarma</option>
+                    <option value="7">Novartis</option>
+                    <option value="8">PT. Sido Muncul Tbk.</option>
+                    <option value="9">Blackmores Limited</option>
+                    <option value="10">H&H Group</option>
+                  </select>
+                </div>
+                <div>
+                  <label for="productcategory" className="text-xl-left">
+                    Product Category
+                  </label>
+                  <select
+                    value={newProduct.addProductCategory}
+                    onChange={inputHandler}
+                    name="addProductCategory"
+                    type="number"
+                    id="productcategory"
+                    placeholder="Antibiotic"
+                    className="form-control"
+                  >
+                    <option value="1">Antibiotics</option>
+                    <option value="2">Antibacterials</option>
+                    <option value="3">Antacids</option>
+                    <option value="4">Antidepressants</option>
+                    <option value="5">Antiarrhythmics</option>
+                    <option value="6">Suplement</option>
+                    <option value="7">Anti-Inflammatories</option>
+                    <option value="8">Antipyretics</option>
+                    <option value="9">Paracetamol</option>
+                    <option value="10">Immunosuppressives</option>
+                  </select>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  onClick={addNewProduct}
+                  className="btn btn-info text-white"
+                >
+                  Add Product
+                </button>
+                <button onClick={cancelEdit} className="btn btn-danger">
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
