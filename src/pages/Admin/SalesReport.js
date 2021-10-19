4b8507af3102e685bd0352af27938069e2154f0a
meysifk <73190163+meysifk@users.noreply.github.com>
2021-10-19 10:16:39 +0700

    FE-Meysi-02: Add Fetch Report Sales function
    Signed-off-by: meysifk <73190163+meysifk@users.noreply.github.com>

--- src/pages/Admin/SalesReport.js
+++ src/pages/Admin/SalesReport.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React,  { useState, useEffect }  from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import Axios from 'axios';
+import { API_URL } from '../../helper';
+
 
 const SalesReport = () => {
 
   const userGlobal = useSelector((state) => state.userGlobal);
+
+  const [salesFetch, setSalesFetch] = useState({
+    salesDataList: []
+  });
+  
+  const fetchSales = () => {
+    Axios.get(`${API_URL}/admin`)
+      .then((res) => {
+        if (res.data.length) {
+          setSalesFetch({ ...salesFetch, salesDataList: res.data });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchSales();
+  }, []);
+
+  const renderReports = () => {
+    let salesDataList = [...salesFetch.salesDataList];
+
+    return salesDataList.map((val) => {
+      return(
+        <tr>
+          <td>{val.Date}</td>
+          <td>{val.product_name}</td>
+          <td>{val.quantity}</td>
+          <td>{val.total_price}</td>
+        </tr>
+      )
+    })
+  }
 
   if (userGlobal?.role !== "admin") {
     return <Redirect to="/" />;
@@ -25,6 +63,7 @@ const SalesReport = () => {
               </tr>
             </thead>
             <tbody>
+              {renderReports()}
             </tbody>
             <tfoot>
             </tfoot>
